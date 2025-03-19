f0bff59b6c1143b5a89370fb4245f83781a35d9e
Marleen <marleeneijkemans@gmail.com>
2025-03-19 10:00:05 +0100

    add homecontroller for testing

---
+++ Eventary-API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("/")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "Welcome to Eventary API!" });
+    }
+}
