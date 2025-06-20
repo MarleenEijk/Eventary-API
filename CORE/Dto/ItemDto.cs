7f686060f07fda94993ff1f0cd7cc626286b2263
Marleen <marleeneijkemans@gmail.com>
2025-06-20 10:04:13 +0200

    Update ItemDto.cs

--- CORE/Dto/ItemDto.cs
+++ CORE/Dto/ItemDto.cs
@@ -5,21 +5,15 @@ namespace CORE.Dto
     public class ItemDto
     {
         [Required] public required long Id { get; set; }
-
         [Required] public required string Name { get; set; }
-
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.01.")]
         public decimal Price { get; set; }
-
         [Required]
         [Range(0, int.MaxValue, ErrorMessage = "Quantity cannot be negative.")]
         public int Quantity { get; set; }
-
         [Required] public required string ImageUrl { get; set; }
-
         [Required] public required long Category_Id { get; set; }
-
         [Required] public required long Company_Id { get; set; }
     }
 }
